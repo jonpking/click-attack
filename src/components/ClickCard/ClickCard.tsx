b32260b33205a1540ca7b53c16a02ca1a1790187
Jon King <jonpking@gmail.com>
2023-07-21 22:24:18 -0700

    add onClick/on start randomization of click cards

--- src/components/ClickCard/ClickCard.tsx
+++ src/components/ClickCard/ClickCard.tsx
@@ -2,10 +2,17 @@ import './ClickCard.css';
 
 interface IProps {
   image: string;
+  onClick: React.MouseEventHandler;
 }
 
-const ClickCard = ({ image }: IProps) => {
-  return <img src={`src/assets/images/${image}.png`} alt={`${image} image`} />;
+const ClickCard = ({ image, onClick }: IProps) => {
+  return (
+    <img
+      onClick={onClick}
+      src={`src/assets/images/${image}.png`}
+      alt={`${image} image`}
+    />
+  );
 };
 
 export default ClickCard;
--- src/components/PlayArea/PlayArea.tsx
+++ src/components/PlayArea/PlayArea.tsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import ClickCard from '../ClickCard/ClickCard';
 import './PlayArea.css';
 
-const heroArray = [
+const seededArray = [
   'antimage',
   'bounty_hunter',
   'bristleback',
@@ -24,12 +25,41 @@ const heroArray = [
   'winter_wyvern',
 ];
 
+const shuffleHeroes = (array: string[]) => {
+  const shuffledArray = [...array];
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [shuffledArray[currentIndex], shuffledArray[randomIndex]] = [
+      shuffledArray[randomIndex],
+      shuffledArray[currentIndex],
+    ];
+  }
+
+  return shuffledArray;
+};
+
 const PlayArea = () => {
+  const [heroArray, setHeroArray] = useState(shuffleHeroes(seededArray));
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setHeroArray(shuffleHeroes(heroArray));
+    console.log(e.target);
+  };
+
   return (
     <div id="playAreaContainer">
       <div id="playArea">
         {heroArray.map((hero) => (
-          <ClickCard image={hero} />
+          <ClickCard onClick={handleClick} image={hero} key={hero} />
         ))}
       </div>
     </div>
